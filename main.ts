ebe88162a87e5d790cb7ba8bdb2974819c9b5d51
hkhs40526 <131329402+hkhs40526@users.noreply.github.com>
2023-04-20 11:55:15 +0800

    少了平手的

--- main.ts
+++ main.ts
@@ -6,9 +6,6 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.showIcon(IconNames.Happy)
         music.playMelody("E F D A G E D C ", 133)
     }
-    if (receivedNumber == K) {
-        basic.showIcon(IconNames.Heart)
-    }
 })
 input.onGesture(Gesture.Shake, function () {
     K = randint(1, 6)
